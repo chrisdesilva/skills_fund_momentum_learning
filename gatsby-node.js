a72241c3bd7b93a8dd26b018b9af3883b09fb5c0
Arthur Kang <akang@goalsolutions.com>
2021-03-29 00:41:46 -0700

    Redirect Site

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,16 +1,16 @@
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage, createRedirect } = actions
+    const { createPage, createRedirect } = actions
 
-  createRedirect({
-    fromPath: "https://momentumlearn.skills.fund/*",
-    toPath: "https://momentum.skills.fund",
-    isPermanent: true,
-    force: true,
-  })
-  createRedirect({
-    fromPath: "/go",
-    toPath:
-      "/?utm_source=school&utm_medium=printedskfbrochure&utm_campaign=studentbrochure",
-  })
-  createRedirect({ fromPath: "/accepted", toPath: "/" })
+    createRedirect({
+        fromPath: "/go",
+        toPath:
+          "/?utm_source=school&utm_medium=printedskfbrochure&utm_campaign=studentbrochure",
+      })
+    createRedirect({
+        fromPath: "/*",
+        toPath: "https://partner.ascentfunding.com/momentumlearn/",
+        isPermanent: true,
+        force: true,
+    })
 }
+
